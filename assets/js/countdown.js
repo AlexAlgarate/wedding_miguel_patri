db139f68c9a35216e66f830496cf0ffdcdce3ec1
Alex Algarate <alex.algarate@gmail.com>
2026-01-22 13:09:21 +0100

    Update countdown function and create uv package mangment

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,35 +1,41 @@
 function getMadridTime(dateString) {
-    return new Date(new Date(dateString).toLocaleString("en-US", { timeZone: "Europe/Madrid" }));
+  return new Date(
+    new Date(dateString).toLocaleString('en-US', { timeZone: 'Europe/Madrid' }),
+  );
 }
-var countDownDate = getMadridTime("May 30, 2025 19:30:00").getTime();
-
+var countDownDate = getMadridTime('May 30, 2025 19:30:00').getTime();
 
 var x = setInterval(function () {
-    var daysElement = document.getElementById("days");
-    var hoursElement = document.getElementById("hours");
-    var minutesElement = document.getElementById("minutes");
-    var secondsElement = document.getElementById("seconds");
+  var daysElement = document.getElementById('days');
+  var hoursElement = document.getElementById('hours');
+  var minutesElement = document.getElementById('minutes');
+  var secondsElement = document.getElementById('seconds');
 
-    if (daysElement !== null && hoursElement !== null && minutesElement !== null && secondsElement !== null) {
-        var now = new Date().getTime();
-        var distance = countDownDate - now;
+  if (
+    daysElement !== null &&
+    hoursElement !== null &&
+    minutesElement !== null &&
+    secondsElement !== null
+  ) {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
 
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        daysElement.innerHTML = days ;
-        hoursElement.innerHTML = hours ;
-        minutesElement.innerHTML = minutes ;
-        secondsElement.innerHTML = seconds ;
+    daysElement.innerHTML = days;
+    hoursElement.innerHTML = hours;
+    minutesElement.innerHTML = minutes;
+    secondsElement.innerHTML = seconds;
 
-        if (distance < 0) {
-            clearInterval(x);
-            daysElement.innerHTML = "¡Comienza nuestra boda!";
-            hoursElement.innerHTML = "";
-            minutesElement.innerHTML = "";
-            secondsElement.innerHTML = "";
-        }
+    if (distance < 0) {
+      clearInterval(x);
+      daysElement.innerHTML = '¡Ya hemos celebrado nuestra boda!';
+      hoursElement.innerHTML = '';
+      minutesElement.innerHTML = '';
+      secondsElement.innerHTML = '';
     }
+  }
 }, 1000);
--- wedding_miguel_patri/views/countdown/countdown.py
+++ wedding_miguel_patri/views/countdown/countdown.py
@@ -2,9 +2,30 @@ import reflex as rx
 
 from wedding_miguel_patri.components import secondary_button, text_section
 from wedding_miguel_patri.styles import Color
-from wedding_miguel_patri.utils import FileRoutes, urls, utils
+from wedding_miguel_patri.styles.fonts import Font, FontHeight, FontWeight
+from wedding_miguel_patri.styles.style import Size
+from wedding_miguel_patri.utils import urls, utils
 
-from .components import create_countdown_vstack
+
+def finished_wedding(text: str) -> rx.Component:
+    return rx.text(
+        text,
+        color=Color.NUMBERS_COUNTDOWN.value,
+        width="100%",
+        height="100%",
+        font_family=Font.TITLE.value,
+        text_align="center",
+        font_style="normal",
+        font_weight=FontWeight.MEDIUM_INSIDE_TEXTS.value,
+        line_height=FontHeight.NORMAL.value,
+        font_size=[
+            Size.BIG.value,
+            Size.BIG.value,
+            Size.BIG.value,
+            Size.BIG.value,
+            Size.BIG.value,
+        ],
+    )
 
 
 def countdown() -> rx.Component:
@@ -14,25 +35,30 @@ def countdown() -> rx.Component:
     Returns:
         rx.Component: The countdown component.
     """
-    countdown_elements = [
-        create_countdown_vstack(element="days", text_date="Días"),
-        create_countdown_vstack(element="hours", text_date="horas"),
-        create_countdown_vstack(element="minutes", text_date="mins"),
-        create_countdown_vstack(element="seconds", text_date="segs"),
-    ]
+    ## The weeding has been already celebrated
+    # countdown_elements = [
+    #     create_countdown_vstack(element="days", text_date="Días"),
+    #     create_countdown_vstack(element="hours", text_date="horas"),
+    #     create_countdown_vstack(element="minutes", text_date="mins"),
+    #     create_countdown_vstack(element="seconds", text_date="segs"),
+    # ]
     return rx.vstack(
         text_section(utils.countdown_text),
         secondary_button(utils.countdown_button, url=urls.CALENDAR_HTML),
         rx.flex(
-            *countdown_elements,
-            rx.script(src=FileRoutes.JS_COUNTDOWN.value),
+            # As the wedding has already celebrated, this lines breake the UI.
+            # *countdown_elements,
+            # rx.script(src=FileRoutes.JS_COUNTDOWN.value),
+            finished_wedding("¡Ya hemos celebrado nuestra boda!"),
+            justify="center",
+            align="center",
             padding="16px 0px",
             gap="1.5em",
             align_self="stretch",
-            flex_direction="row",
             background=Color.COUNTDOWN_BACKGROUND.value,
             width="100%",
         ),
+        align="center",
         gap="16px",
         id="wedding_date",
         width="100%",
